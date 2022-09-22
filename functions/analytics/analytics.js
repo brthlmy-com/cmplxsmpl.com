d752454783bbdf760bf77d1bd535a02a3704aa32
Eric Bartholemy <eric.bartholemy@gmail.com>
2022-09-22 16:43:37 +0200

    adds analytics endpoint

---
+++ functions/analytics/analytics.js
@@ -0,0 +1,50 @@
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const {
+  GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  GOOGLE_PRIVATE_KEY,
+  SPREADSHEET_ID,
+  SPREADSHEET_SHEET_TITLE
+} = process.env;
+
+exports.handler = async (event, context) => {
+  if (
+    GOOGLE_SERVICE_ACCOUNT_EMAIL &&
+    GOOGLE_PRIVATE_KEY &&
+    SPREADSHEET_ID &&
+    SPREADSHEET_SHEET_TITLE
+  ) {
+    try {
+      // google-spreadsheet
+      const client_email = GOOGLE_SERVICE_ACCOUNT_EMAIL;
+      const private_key = GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n");
+
+      const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+      await doc.useServiceAccountAuth({ client_email, private_key });
+      await doc.loadInfo();
+
+      // sheet
+      const sheet = doc.sheetsByTitle[SPREADSHEET_SHEET_TITLE];
+
+      // get rows
+      const rows = await sheet.getRows();
+
+      // return json response
+      console.log(rows);
+
+
+    } catch (error) {
+      console.error(`[ERROR]`, error);
+    }
+  } else {
+    console.log(
+      `[ENV] GOOGLE_SERVICE_ACCOUNT_EMAIL && GOOGLE_PRIVATE_KEY && SPREADSHEET_ID && SPREADSHEET_SHEET_TITLE`
+    );
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      result: JSON.stringify(rows)
+    })
+  }
+};
