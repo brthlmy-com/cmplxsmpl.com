7274ce5662f22a81cdc42ff80e886ffa9f97dad5
Eric Bartholemy <eric.bartholemy@gmail.com>
2023-03-10 17:25:52 +0100

    adds workflow for fetch yt stats

---
+++ sh/fetch-yt-stats/index.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+const ytdl = require('ytdl-core');
+const ytpl = require('ytpl');
+const fs = require('fs');
+const _ = require('lodash');
+
+// data is piped in from shell
+// since curl is easier than https.get
+let input = '';
+let output = {};
+
+const fetchVideoInfo = async youtubeUrl => {
+  const {videoDetails} = await ytdl.getInfo(youtubeUrl);
+
+  const {
+    videoId,
+    viewCount,
+    likes,
+    dislikes,
+    title,
+    description,
+    lengthSeconds,
+    category,
+    uploadDate,
+    publishDate,
+    isPrivate,
+    isUnlisted,
+    isFamilySafe,
+    availableCountries,
+    author: {verified, subscriber_count},
+  } = videoDetails;
+
+  const info = {
+    timestamp: new Date().toISOString(),
+    videoId,
+    lengthSeconds,
+    viewCount,
+    likes,
+    dislikes,
+    title,
+    description,
+    category,
+    uploadDate,
+    publishDate,
+    isPrivate,
+    isUnlisted,
+    isFamilySafe,
+    verified,
+    subscriber_count,
+    availableCountries: availableCountries.length,
+  };
+
+  console.log('***', info.videoId, info.timestamp, info.title);
+
+  return info;
+};
+
+process.stdin.resume();
+process.stdin.on('data', data => (input += data));
+process.stdin.on('end', async () => {
+  let entries;
+  try {
+    entries = JSON.parse(input);
+
+    const ytChannelVideos = await ytpl('UC_gtVpAyiUt7vcqS5mCpVgw');
+    console.log(
+      '#',
+      ytChannelVideos.estimatedItemCount,
+      ytChannelVideos.title,
+      ytChannelVideos.id,
+      ytChannelVideos.lastUpdated,
+    );
+
+    const ytFetched = ytChannelVideos.items.map(async ytVideo => {
+      console.log(
+        '**',
+        ytVideo.index,
+        ytVideo.title,
+        ytVideo.id,
+        ytVideo.shortUrl,
+      );
+      return fetchVideoInfo(ytVideo.shortUrl);
+    });
+
+    console.log('#', 'Fetching', ytFetched.length);
+
+    const resolved = await Promise.all(ytFetched);
+
+    const ytVideoIdStats = resolved.map(item => item);
+
+    const output = ytVideoIdStats.reduce(function(result, current) {
+      return Object.assign(result, {
+        [current.videoId]: {[current.timestamp]: {...current}},
+      });
+    }, {});
+
+    const updated = _.merge(entries, output);
+
+    // fs.writeFileSync('./yt-stats.txt', JSON.stringify(updated, null, 4));
+    process.stdout.write(JSON.stringify(updated, null, 4));
+  } catch (error) {
+    console.error('JSON parse failed', error);
+    console.error(input);
+    process.exit(1);
+  }
+});
