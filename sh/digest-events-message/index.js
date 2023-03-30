cd29c4fcf2f0ef6c74cb017480d190e5d28f5733
Eric Bartholemy <eric.bartholemy@gmail.com>
2023-03-30 18:48:31 +0200

    format message output

--- sh/digest-events-message/index.js
+++ sh/digest-events-message/index.js
@@ -4,14 +4,19 @@ const args = process.argv.slice(2);
 const inputJSON = args[0];
 const $ = JSON.parse(fs.readFileSync(inputJSON));
 
-const $sorted = $.filter(item => item.startDate).sort((a,b) => b.startDate.startDate - a.startDate.startDate);
+const $sorted = $.filter(item => item.startDate).sort(
+  (a, b) => b.startDate.startDate - a.startDate.startDate,
+);
 
 const events = $sorted.map(item => {
   const slugs = item.categorySlugs.map(text => text.toUpperCase()).join(' #');
-  const message = `${item.startDate.text}: <b>${item.title}</b> #${slugs} @[${item.place}]`;
+  const message = `${item.startDate.text} @ ${item.place}: #${slugs} \n <b>${item.title}</b>`;
   return message;
 });
 
-const output = events.slice(0,12).sort().join('\n\n');
+const output = `Hey, today are ${events.length} in Aachen \n\n ${events
+  .slice(0, 12)
+  .sort()
+  .join('\n\n')}`;
 
 process.stdout.write(encodeURIComponent(output));
