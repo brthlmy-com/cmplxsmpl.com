8921c047aae1a9accbfd580b05fbb8cecadaac7b
Eric Bartholemy <eric.bartholemy@gmail.com>
2023-04-16 13:25:09 +0200

    fixes handeling new released videos where 2 weeks ago no data
    * improves message text, shorten
    * subscriber count
    * published videos content length

--- sh/digest-youtube-message/index.js
+++ sh/digest-youtube-message/index.js
@@ -3,6 +3,24 @@ var fs = require('fs');
 const args = process.argv.slice(2);
 const inputJSON = args[0];
 const $ = JSON.parse(fs.readFileSync(inputJSON));
+const humanSeconds = ( value, word = true ) => {
+    with( new Date( value * 1000) )
+    var dd = getUTCDate() - 1,
+        hh = getUTCHours(),
+        mm = getUTCMinutes(),
+        ss = getUTCSeconds(),
+        ml = getUTCMilliseconds(),
+    dg = toUTCString().match(/\d{2}:\d{2}:\d{2}/);
+
+ return word ?
+        (dd ? dd + " days " : "") +
+        (hh ? hh + " hours ": "") +
+        (mm ? mm + " minutes ": "") +
+        (ss ? ss + " seconds ": "") +
+        (ml ? ml + " miliseconds " : "" )
+        :
+        (dd ? dd + ":" : "") + dg[0] + (ml ? "." + ml : "");
+  };
 
 const ytVideos = Object.keys($).map(item => item);
 
@@ -13,12 +31,17 @@ const ytVideoViewCount = ytVideos.map(video => {
   const twoWeeksAgoRecord = records[records.length - 14];
 
   const today = $[video][todayRecord];
-  const lastWeek = $[video][twoWeeksAgoRecord];
+  // twoWeeksAgoRecord does not exist, take today
+  const lastWeek = twoWeeksAgoRecord
+    ? $[video][twoWeeksAgoRecord]
+    : {...today, likes: null, viewCount: 0};
 
   return {
     title: today.title.slice(0, 30),
     date: todayRecord,
     likes: today.likes || 0,
+    subscriberCount: today["subscriber_count"],
+    lengthSeconds: today.lengthSeconds,
     publishDate: today.publishDate,
     plusLikes: (today.likes || 0) - (lastWeek.likes || 0),
     viewCount: today.viewCount,
@@ -59,8 +82,8 @@ const likedCountVideos = ytVideoViewCount
 const likedCountVideosText = likedCountVideos
   .map(
     item =>
-      `"${item.title}" published on ${item.publishDate} ${item.likes ||
-        0} people liked, that are ${item.plusLikes} more than 2 weeks ago.`,
+      `"${item.title}" published on ${item.publishDate} liked ${item.likes ||
+        0} people, ${item.plusLikes} more.`,
   )
   .slice(0, 3)
   .join('\n\n');
@@ -69,12 +92,12 @@ const mostViewsVideos = changeCountVideos
   .sort((a, b) => b.plusViews - a.plusViews)
   .map(
     item =>
-      `"${item.title}" published on ${item.publishDate} ${
+      `"${item.title}" published on ${item.publishDate} watched x ${
         item.viewCount
-      }x watched, that are ${
+      }, ${
         item.plusViews
-      } more views than 2 weeks ago. ${item.likes ||
-        0} people liked, that are ${item.plusLikes} more than 2 weeks ago.`,
+      } more, ${item.likes ||
+        0} people liked, ${item.plusLikes} more.`,
   );
 
 const mostViews = mostViewsVideos.slice(0, 3).join('\n\n');
@@ -83,30 +106,35 @@ const todayVideos = ytVideoViewCount
   .sort((a, b) => b.viewCount - a.viewCount)
   .map(
     item =>
-      `"${item.title}" published on ${item.publishDate} ${item.viewCount}x watched.`,
+      `"${item.title}" published on ${item.publishDate} watched ${item.viewCount} views.`,
   )
   .slice(0, 5)
   .join('\n\n');
 
-const totalPlusLikes = ytVideoViewCount.map(item => item.plusLikes).reduce(
-  (a, b) => a + b,
-  0,
-);
+const totalPlusLikes = ytVideoViewCount
+  .map(item => item.plusLikes)
+  .reduce((a, b) => a + b, 0);
 
-const totalPlusViews = ytVideoViewCount.map(item => parseInt(item.plusViews)).reduce(
-  (a, b) => a + b,
-  0,
-);
+const totalPlusViews = ytVideoViewCount
+  .map(item => parseInt(item.plusViews))
+  .reduce((a, b) => a + b, 0);
 
-const channelViews = ytVideoViewCount.map(item => parseInt(item.viewCount)).reduce(
-  (a, b) => a + b,
-  0,
-);
+const channelViews = ytVideoViewCount
+  .map(item => parseInt(item.viewCount))
+  .reduce((a, b) => a + b, 0);
 
-const totalLikes = ytVideoViewCount.map(item => parseInt(item.likes)).reduce(
-  (a, b) => a + b,
-  0,
-);
+const totalLikes = ytVideoViewCount
+  .map(item => parseInt(item.likes))
+  .reduce((a, b) => a + b, 0);
+
+const subscriberCount = ytVideoViewCount
+  .map(item => parseInt(item.subscriberCount))
+  .reduce((a, b) => b = a > b ? a : b,  0);
+
+const contentLength = ytVideoViewCount
+  .map(item => parseInt(item.lengthSeconds))
+  .reduce((a, b) => a + b, 0);
+
 const output = `These are the latest 3 published videos: \n
 ${latestViewCountsText}\n
 Compared with today and two weeks ago.\n
@@ -116,13 +144,13 @@ These received the most views,  ${mostViewsVideos.length} videos watched, for a 
 ${mostViews}\n
 Of ${ytVideoViewCount.length} videos have the most views today.\n
 ${todayVideos}\n
-Channel has ${channelViews} views and ${totalLikes} video likes.
+Channel has ${channelViews} views, ${ytVideoViewCount.length} videos (${humanSeconds(contentLength)}), ${totalLikes} video likes and ${subscriberCount} subscribers.
 `;
 
 // console.log(
   // // changeCountVideos.length,
   // // ytVideoViewCount.length,
-  // likedCountVideos,
+  // // likedCountVideos,
   // output,
   // output.length,
 // );
