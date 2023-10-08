af46cd326ce4923a0a8ca7fd19b7c9411149965c
Eric Bartholemy <eric.bartholemy@gmail.com>
2023-10-08 18:48:20 +0200

    feat: updates youtube digest message

--- sh/digest-youtube-message/index.js
+++ sh/digest-youtube-message/index.js
@@ -3,155 +3,145 @@ var fs = require('fs');
 const args = process.argv.slice(2);
 const inputJSON = args[0];
 const $ = JSON.parse(fs.readFileSync(inputJSON));
-const humanSeconds = ( value, word = true ) => {
-    with( new Date( value * 1000) )
+const humanSeconds = (value, word = true) => {
+  with (new Date(value * 1000))
     var dd = getUTCDate() - 1,
-        hh = getUTCHours(),
-        mm = getUTCMinutes(),
-        ss = getUTCSeconds(),
-        ml = getUTCMilliseconds(),
-    dg = toUTCString().match(/\d{2}:\d{2}:\d{2}/);
+      hh = getUTCHours(),
+      mm = getUTCMinutes(),
+      ss = getUTCSeconds(),
+      ml = getUTCMilliseconds(),
+      dg = toUTCString().match(/\d{2}:\d{2}:\d{2}/);
 
- return word ?
-        (dd ? dd + " days " : "") +
-        (hh ? hh + " hours ": "") +
-        (mm ? mm + " minutes ": "") +
-        (ss ? ss + " seconds ": "") +
-        (ml ? ml + " miliseconds " : "" )
-        :
-        (dd ? dd + ":" : "") + dg[0] + (ml ? "." + ml : "");
-  };
+  return word
+    ? (dd ? dd + ' days ' : '') +
+        (hh ? hh + ' hours ' : '') +
+        (mm ? mm + ' minutes ' : '') +
+        (ss ? ss + ' seconds ' : '') +
+        (ml ? ml + ' miliseconds ' : '')
+    : (dd ? dd + ':' : '') + dg[0] + (ml ? '.' + ml : '');
+};
 
-const ytVideos = Object.keys($).map(item => item);
+const ytHref = videoId => {
+  return `https://www.youtube.com/watch?v=${videoId}`;
+};
 
-const ytVideoViewCount = ytVideos.map(video => {
+const timeSince = date => {
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + ' years';
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + ' months';
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + ' days';
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + ' hours';
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + ' minutes';
+  }
+  return Math.floor(seconds) + ' seconds';
+};
+
+const buildRecord = (video, daysAgoInt) => {
   const records = Object.keys($[video]);
-
-  const todayRecord = records[records.length - 1];
-  const twoWeeksAgoRecord = records[records.length - 14];
-
+  const todayRecord = records[records.length - daysAgoInt];
   const today = $[video][todayRecord];
-  // twoWeeksAgoRecord does not exist, take today
-  const lastWeek = twoWeeksAgoRecord
-    ? $[video][twoWeeksAgoRecord]
-    : {...today, likes: null, viewCount: 0};
 
   return {
+    id: today.videoId,
+    url: ytHref(today.videoId),
     title: today.title.slice(0, 30),
     date: todayRecord,
     likes: today.likes || 0,
-    subscriberCount: today["subscriber_count"],
+    subscriberCount: today['subscriber_count'],
     lengthSeconds: today.lengthSeconds,
     publishDate: today.publishDate,
-    plusLikes: (today.likes || 0) - (lastWeek.likes || 0),
+    timeSince: timeSince(new Date(today.publishDate)),
     viewCount: today.viewCount,
-    plusViews: today.viewCount - lastWeek.viewCount,
-    twoWeeksAgo: {
-      date: twoWeeksAgoRecord,
-      likes: lastWeek.likes || 0,
-      viewCount: lastWeek.viewCount,
-      viewerCount: today.viewCount - lastWeek.viewCount,
-      data: lastWeek,
-    },
   };
-});
+};
 
-// video was watched within two weeks
-const changeCountVideos = ytVideoViewCount.filter(
-  item => item.viewCount - item.twoWeeksAgo.viewCount > 0,
-);
+const ytVideos = Object.keys($).map(item => item);
 
-// info about latest videos published
-const latestViewCounts = changeCountVideos
-  .sort((a, b) => new Date(b.publishDate) - new Date(a.publishDate))
-  .map(
-    item =>
-      `"${item.title}" published on ${item.publishDate}, ${
-        item.viewCount
-      }x viewed. ${item.likes || 0} people liked`,
-  );
+const aWeekAgoVideos = ytVideos
+  .filter(video => Object.keys($[video]).length > 7) // has yesterday
+  .map(video => buildRecord(video, 7));
 
-const latestViewCountsText = latestViewCounts.slice(0, 3).join('\n\n');
+const yesterdayVideos = ytVideos
+  .filter(video => Object.keys($[video]).length > 1) // has yesterday
+  .map(video => buildRecord(video, 2));
 
-const likedCountVideos = ytVideoViewCount
-  .filter(item => {
-    return (item.likes || 0) - (item.twoWeeksAgo.likes || 0) > 0;
-  })
-  .sort((a, b) => b.plusLikes - a.plusLikes);
+const ytVideoViewCount = ytVideos.map(video => buildRecord(video, 1));
 
-const likedCountVideosText = likedCountVideos
-  .map(
-    item =>
-      `"${item.title}" published on ${item.publishDate} liked ${item.likes ||
-        0} people, ${item.plusLikes} more.`,
-  )
-  .slice(0, 3)
-  .join('\n\n');
-
-const mostViewsVideos = changeCountVideos
-  .sort((a, b) => b.plusViews - a.plusViews)
-  .map(
-    item =>
-      `"${item.title}" published on ${item.publishDate} watched x ${
-        item.viewCount
-      }, ${
-        item.plusViews
-      } more, ${item.likes ||
-        0} people liked, ${item.plusLikes} more.`,
-  );
-
-const mostViews = mostViewsVideos.slice(0, 3).join('\n\n');
-
-const todayVideos = ytVideoViewCount
+const videosTop5 = ytVideoViewCount
   .sort((a, b) => b.viewCount - a.viewCount)
   .map(
     item =>
-      `"${item.title}" published on ${item.publishDate} watched ${item.viewCount} views.`,
+      `#${item.id} | ${item.viewCount} | <a href="${item.url}">${item.title}</a> | ${item.timeSince}`,
   )
   .slice(0, 5)
   .join('\n\n');
 
-const totalPlusLikes = ytVideoViewCount
-  .map(item => item.plusLikes)
-  .reduce((a, b) => a + b, 0);
+const mostRecent = ytVideoViewCount.sort(
+  (a, b) => new Date(b.publishDate) - new Date(a.publishDate),
+);
 
-const totalPlusViews = ytVideoViewCount
-  .map(item => parseInt(item.plusViews))
-  .reduce((a, b) => a + b, 0);
+const videosRecently = mostRecent
+  .map(
+    item =>
+      `#${item.id} | ${item.viewCount} | <a href="${item.url}">${item.title}</a> | ${item.timeSince}`,
+  )
+  .slice(0, 5)
+  .join('\n\n');
 
 const channelViews = ytVideoViewCount
   .map(item => parseInt(item.viewCount))
   .reduce((a, b) => a + b, 0);
 
-const totalLikes = ytVideoViewCount
-  .map(item => parseInt(item.likes))
+const channelViewsYesterday = yesterdayVideos
+  .map(item => parseInt(item.viewCount))
+  .reduce((a, b) => a + b, 0);
+
+const channelViewsWeekAgo = aWeekAgoVideos
+  .map(item => parseInt(item.viewCount))
   .reduce((a, b) => a + b, 0);
 
 const subscriberCount = ytVideoViewCount
   .map(item => parseInt(item.subscriberCount))
-  .reduce((a, b) => b = a > b ? a : b,  0);
+  .reduce((a, b) => (b = a > b ? a : b), 0);
 
 const contentLength = ytVideoViewCount
   .map(item => parseInt(item.lengthSeconds))
   .reduce((a, b) => a + b, 0);
 
-const output = `These are the latest 3 published videos: \n
-${latestViewCountsText}\n
-Compared with today and two weeks ago.\n
-These received the most new likes, ${likedCountVideos.length} videos with total of ${totalPlusLikes} likes:\n
-${likedCountVideosText}\n
-These received the most views,  ${mostViewsVideos.length} videos watched, for a total of ${totalPlusViews} views:\n
-${mostViews}\n
-Of ${ytVideoViewCount.length} videos have the most views today.\n
-${todayVideos}\n
-Channel has ${channelViews} views, ${ytVideoViewCount.length} videos (${humanSeconds(contentLength)}), ${totalLikes} video likes and ${subscriberCount} subscribers.
+const output = `#youtube #monitoring\n
+https://youtube.com/@_mesotv_\n
+#Support ${subscriberCount}\n
+#Watched ${channelViews} | Yesterday ${channelViewsYesterday} +${channelViews - channelViewsYesterday} | 7 days ago ${channelViewsWeekAgo} +${channelViews - channelViewsWeekAgo}\n
+#NewVideo ${mostRecent[0].timeSince} ago\n
+#TotalVideo ${ytVideoViewCount.length} | #ContentHours ${humanSeconds(
+  contentLength,
+)}\n
+Recently released:\n
+${videosRecently}\n
+Most watched videos:\n
+${videosTop5}\n
 `;
 
-// console.log(
-  // // changeCountVideos.length,
-  // // ytVideoViewCount.length,
-  // // likedCountVideos,
-  // output,
-  // output.length,
-// );
+console.log(
+  // changeCountVideos.length,
+  // ytVideoViewCount.length,
+  output,
+  output.length,
+);
 process.stdout.write(encodeURIComponent(output));
